0b9d32fc78bd1ab85ff3250a72abc2933b40d1c8
rimsha_bibi <108316811+Rimshashehzadi@users.noreply.github.com>
2026-08-15 01:04:50 -0700

    ADD

---
+++ it_services/src/app/components/home/Home1.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Hero from './Hero'
+import TrustedBy from './TrustedBy'
+import Services from './Services'
+import Technologies from './Technologies'
+import StatsSection from './StatsSection'
+import Projects from './Projects'
+import WhyChooseUs from './WhyChooseUs'
+import Testimonials from './Testimonials'
+import Plans from './Plans'
+import Contact from './Contact'
+import BottomGallery from './BottomGallery'
+
+const Home1 = () => {
+  return (
+    <div>
+        <Hero/>
+        <TrustedBy/>
+        <Services/>
+        <Technologies/>
+        <StatsSection/>
+        <Projects/>
+        <WhyChooseUs/>
+        <Testimonials/>
+        <Plans/>
+        <Contact/>
+        <BottomGallery/>
+    </div>
+  )
+}
+
+export default Home1
--- it_services/src/app/page.js
+++ it_services/src/app/page.js
@@ -1,29 +1,8 @@
-import Hero from "../app/components/home/Hero";
-import TrustedBy from "../app/components/home/TrustedBy";
-import Services from "../app/components/home/Services";
-import Technologies from "../app/components/home/Technologies";
-import StatsSection from "../app/components/home/StatsSection";
-import Projects from "../app/components/home/Projects";
-import WhyChooseUs from "../app/components/home/WhyChooseUs";
-import Testimonials from "../app/components/home/Testimonials";
-import Plans from "../app/components/home/Plans";
-import Contact from "../app/components/home/Contact";
-import BottomGallery from "../app/components/home/BottomGallery";
-
+import Home1 from './components/home/Home1'
 export default function Home() {
   return (
     <main>
-      <Hero />
-      <TrustedBy />
-      <Services />
-      <Technologies />
-      <StatsSection />
-      <Projects />
-      <WhyChooseUs />
-      <Testimonials />
-      <Plans />
-      <Contact />
-      <BottomGallery />
+      <Home1/>
     </main>
   );
 }
